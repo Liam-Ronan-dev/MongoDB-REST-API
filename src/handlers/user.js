e089f041103ab7eae9c1c29a373d91ec23468d79
Liam Ronan <liamronan16@gmail.com>
2024-09-30 18:25:55 +0100

    added user model, authentication using JWTs, configured prettier, connected to MongoDB database

---
+++ src/handlers/user.js
@@ -0,0 +1,45 @@
+import { User } from '../models/userModel.js';
+import { hashPassword, createJWT, comparePassword } from '../modules/auth.js';
+
+export const createUser = async (req, res, next) => {
+  try {
+    const hashedPassword = await hashPassword(req.body.password);
+
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const token = createJWT(user);
+    await user.save();
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    next(error);
+  }
+};
+
+// Sign-in function
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = createJWT(user);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { protect } from './modules/auth.js';
+import { router } from './router.js';
+import { createUser, signin } from './handlers/user.js';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/', (req, res) => {
+//   res.send('hello world');
+// });
+app.use('/api', protect, router);
+app.post('/user', createUser);
+app.post('/signin', signin);
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port: http://localhost:${process.env.PORT}`);
+});
